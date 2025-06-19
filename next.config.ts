f392097d4bf8e83bef8ca6d610d532f73ef0ce48
NawrizTurjo <nawriz_ahmed03@hotmail.com>
2025-06-19 06:52:19 +0600

    config added for deploy

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,8 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  basePath: '/reversi-game', // replace <your-repo-name> with your actual repo
+  assetPrefix: '/reversi-game/', // to load assets correctly
 };
 
 export default nextConfig;
